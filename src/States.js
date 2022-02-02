44c493e6480394de3a7a5106e9287791f802f4ed
sanjibsunahcl <98376370+sanjibsunahcl@users.noreply.github.com>
2022-02-02 16:08:35 +0530

    Set up states

---
+++ src/States.js
@@ -0,0 +1,20 @@
+
+import React, { Component } from 'react'
+import { Text, View } from 'react-native'
+
+class State extends Component {
+   state = {
+      text: 'Click here'
+   }
+   updateState = () ⇒ this.setState({ text: 'The state is updated' })
+   render() {
+      return (
+         <View>
+            <Text onPress = {this.updateState}>
+               {this.state.myState}
+            </Text>
+         </View>
+      );
+   }
+}
+export default State;
