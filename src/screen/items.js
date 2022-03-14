16f2c97f9db6dbefd8da8ac3875bdebacec6f6ad
sanjibsunahcl <98376370+sanjibsunahcl@users.noreply.github.com>
2022-03-14 14:36:09 +0530

    Update items.js

--- src/screen/items.js
+++ src/screen/items.js
@@ -1,15 +1,29 @@
-import React, {Component} from 'react';
-import {Text, View} from 'react-native';
+import React, {useState, useEffect} from 'react';
+import {Text, View, Button} from 'react-native';
 
-class Items extends Component {
-  render() {
-    return (
-      <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-        <Text>Sanjib</Text>
-        <Text style={{fontSize: 18, color: 'black'}}>Items</Text>
-      </View>
-    );
-  }
-}
+const Items = () => {
+  const [status, setStatus] = useState('');
+  const [timeoutStatus, settimeoutStatus] = useState('');
+
+  useEffect(() => {
+    setTimeout(() => {
+      settimeoutStatus('timeout is called');
+    }, 1000);
+    console.log('effect is called');
+  }, []);
+
+  return (
+    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+      <Text>Sanjib</Text>
+      <Text style={{fontSize: 18, color: 'black'}}>Items</Text>
+      <Text testID="timeOutText">{timeoutStatus}</Text>
+      <Text testID="myText">{status}</Text>
+      <Button
+        testID="myButton"
+        onPress={() => setStatus('button pressed')}
+        title="Click"></Button>
+    </View>
+  );
+};
 
 export default Items;
