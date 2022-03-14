15ba646e336307722f5e41809775b29d21dc68f5
sanjibsunahcl <98376370+sanjibsunahcl@users.noreply.github.com>
2022-03-14 14:38:08 +0530

    Update item.test.js

--- __tests__/item.test.js
+++ __tests__/item.test.js
@@ -1,8 +1,26 @@
 import React from 'react';
-import renderer from 'react-test-renderer';
+import {create, act} from 'react-test-renderer';
 import Items from '../src/screen/items';
 
+const tree = create(<Items />);
+
+// jest.runAllTimers();
+//Snap shot test
 it('Item snapshot', () => {
-  const snap = renderer.create(<Items />).toJSON();
-  expect(snap).toMatchSnapshot();
+  expect(tree).toMatchSnapshot();
+});
+
+test('button press', () => {
+  //button press
+  const button = tree.root.findByProps({testID: 'myButton'}).props;
+  act(() => button.onPress());
+
+  const text = tree.root.findByProps({testID: 'myText'}).props;
+  expect(text.children).toEqual('button pressed');
+});
+
+test('call timeout', () => {
+  act(() => jest.runAllTimers());  
+  const text = tree.root.findByProps({testID: 'timeOutText'}).props;
+  expect(text.children).toEqual('timeout is called');
 });
